4a14c1163d3cb166d6de05759d25aa1ca93f067d
phuennae <113125472+phuennae@users.noreply.github.com>
2025-12-17 00:58:40 +0700

    Delete OrderAssignment directory

--- OrderAssignment/emailer.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC, abstractmethod
-
-class EmailService(ABC):
-    @abstractmethod
-    def send(self, to: str, subject: str, body: str) -> None: ...
--- OrderAssignment/shipping.py
+++
@@ -1,5 +0,0 @@
-class ShippingService:
-    def cost(self, total_weight: float, region: str) -> float:
-        if region == "TH":
-            return 50.0 if total_weight <= 5 else 120.0
-        return 300.0
